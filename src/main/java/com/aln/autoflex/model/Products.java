b80eff25f713a8b39ef0f3e864022bd8dac47480
alnsimao <alansimao9a1@gmail.com>
2026-02-24 22:10:33 -0300

    feat(Products: Created products entity model)

---
+++ src/main/java/com/aln/autoflex/model/Products.java
@@ -0,0 +1,32 @@
+package com.aln.autoflex.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "products")
+public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,length = 255)
+    private String name;
+
+    @Column(nullable = false,precision = 10,scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false,updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
