615a12411177e0595be8dde747f76162aee095cc
alnsimao <alansimao9a1@gmail.com>
2026-02-25 22:19:36 -0300

    Product: Add unique = true to field name)

--- src/main/java/com/aln/autoflex/model/Product.java
+++ src/main/java/com/aln/autoflex/model/Product.java
@@ -19,7 +19,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false,length = 255)
+    @Column(nullable = false,length = 255, unique = true)
     private String name;
 
     @Column(nullable = false,precision = 10,scale = 2)
