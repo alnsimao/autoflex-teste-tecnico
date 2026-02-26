b7dca8a9a1b4bdce296a3f98bf5578a2decf82cb
alnsimao <alansimao9a1@gmail.com>
2026-02-26 18:04:59 -0300

    feat(ProductionService: created the CRUD for ProductionService)

--- src/main/java/com/aln/autoflex/repository/ProductCompositionRepository.java
+++ src/main/java/com/aln/autoflex/repository/ProductCompositionRepository.java
@@ -1,9 +1,15 @@
 package com.aln.autoflex.repository;
 
+import com.aln.autoflex.dto.ProductCompositionResponseDTO;
 import com.aln.autoflex.model.ProductComposition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface ProductCompositionRepository extends JpaRepository<ProductComposition, Long> {
+    List<ProductComposition> findAllByProductId(Long id);
+
 }
---
+++ src/main/java/com/aln/autoflex/service/ProductionService.java
@@ -0,0 +1,104 @@
+package com.aln.autoflex.service;
+
+
+import com.aln.autoflex.dto.ProductCompositionRequestDTO;
+import com.aln.autoflex.dto.ProductCompositionResponseDTO;
+import com.aln.autoflex.exceptions.ResourceNotFoundException;
+import com.aln.autoflex.model.Product;
+import com.aln.autoflex.model.ProductComposition;
+import com.aln.autoflex.model.RawMaterial;
+import com.aln.autoflex.repository.ProductCompositionRepository;
+import com.aln.autoflex.repository.ProductRepository;
+import com.aln.autoflex.repository.RawMaterialRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductionService {
+    private final ProductRepository productRepository;
+    private final RawMaterialRepository materialRepository;
+    private final ProductCompositionRepository compositionRepository;
+
+    @Transactional
+    public ProductCompositionResponseDTO create(ProductCompositionRequestDTO productCompositionRequestDTO) {
+
+        Product product = productRepository.findById(productCompositionRequestDTO.productId()).orElseThrow
+                (
+                        ()-> new ResourceNotFoundException("Product not found")
+                );
+
+        RawMaterial material = materialRepository.findById(productCompositionRequestDTO.rawMaterialId()).orElseThrow
+                        (
+                                ()-> new ResourceNotFoundException("Material not found")
+                        );
+
+        ProductComposition productComposition = new ProductComposition();
+        productComposition.setProduct(product);
+        productComposition.setRawMaterial(material);
+        productComposition.setQuantityNeeded(productCompositionRequestDTO.quantityNeeded());
+
+        compositionRepository.save(productComposition);
+        return toDTO(productComposition);
+
+    }
+    @Transactional
+    public ProductCompositionResponseDTO update(Long id, ProductCompositionRequestDTO productCompositionRequestDTO) {
+        ProductComposition productComposition = compositionRepository.findById(id).orElseThrow
+                (
+                        ()-> new ResourceNotFoundException("Product composition not found")
+                );
+        Product product = productRepository.findById(id).orElseThrow
+                (
+                        ()-> new ResourceNotFoundException("Product not found")
+                );
+        RawMaterial rawMaterial = materialRepository.findById(productCompositionRequestDTO.rawMaterialId())
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("Material not found")
+                );
+        productComposition.setProduct(product);
+        productComposition.setRawMaterial(rawMaterial);
+        productComposition.setQuantityNeeded(productCompositionRequestDTO.quantityNeeded());
+
+        return toDTO(productComposition);
+    }
+    @Transactional
+    public void delete(Long id) {
+            ProductComposition productComposition = compositionRepository.findById(id).orElseThrow
+                    (
+                            ()-> new ResourceNotFoundException("Product composition not found")
+                    );
+            compositionRepository.delete(productComposition);
+    }
+
+    @Transactional
+    public ProductCompositionResponseDTO findById(Long id) {
+        ProductComposition productComposition = compositionRepository.findById(id).orElseThrow
+                (()-> new ResourceNotFoundException("Product composition not found"));
+
+        return toDTO(productComposition);
+    }
+
+    @Transactional
+    public List<ProductCompositionResponseDTO> findByProductId(Long productId) {
+        return compositionRepository.findAllByProductId(productId)
+                .stream()
+                .map(this::toDTO)
+                .toList();
+    }
+
+    private ProductCompositionResponseDTO toDTO(ProductComposition productComposition) {
+        return new ProductCompositionResponseDTO(
+                productComposition.getId(),
+                productComposition.getProduct().getId(),
+                productComposition.getProduct().getName(),
+                productComposition.getRawMaterial().getId(),
+                productComposition.getRawMaterial().getName(),
+                productComposition.getQuantityNeeded()
+        );
+    }
+
+}
